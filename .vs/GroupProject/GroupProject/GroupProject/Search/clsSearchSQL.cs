6af40b2d190642786322b8568edc167c9c346249
Matthew Johnson <matthewjohnson22@mail.weber.edu>
2021-03-30 12:14:13 -0600

    Finished search screen prototype.

--- .vs/GroupProject/GroupProject/GroupProject/Search/clsSearchSQL.cs
+++ .vs/GroupProject/GroupProject/GroupProject/Search/clsSearchSQL.cs
@@ -6,7 +6,101 @@ using System.Threading.Tasks;
 
 namespace GroupProject.Search
 {
-    class clsSearchSQL
+    public class clsSearchSQL
     {
+
+        /// <summary>
+        /// The SQL gets all of the invoices.
+        /// </summary>
+        /// <returns></returns>
+        public string SelectAllInvoices()
+        {
+            string sSQL = "SELECT * FROM Invoices";
+
+            return sSQL;
+        }
+
+        /// <summary>
+        /// The SQL to select all the inoices with a specific invoice number.
+        /// </summary>
+        /// <param name="InvoiceNum">The InvoiceNum to retrieve the invoice data</param>
+        /// <returns></returns>
+        public string SelectInvoiceData(string InvoiceNum)
+        {
+            string sSQL = "SELECT * FROM Invoices WHERE InvoiceNum = " + InvoiceNum;
+
+            return sSQL;
+        }
+
+        /// <summary>
+        /// The SQL returns the specific invoices with the provided invoice number and date.
+        /// </summary>
+        /// <param name="InvoiceNum">The invoice number</param>
+        /// <param name="InvoiceDate">The invoice date</param>
+        /// <returns></returns>
+        public string SelectInvoiceByDate(string InvoiceNum, string InvoiceDate)
+        {
+            string sSQL = "SELECT * FROM Invoices WHERE InvoiceNum = " + InvoiceNum + "AND InvoiceDate = #" + InvoiceDate + "#";
+
+            return sSQL;
+        }
+
+        /// <summary>
+        /// The SQL returns the specific invoices with the provided invoice number,date and total cost.
+        /// </summary>
+        /// <param name="InvoiceNum">The invoice number</param>
+        /// <param name="InvoiceDate">The invoice date</param>
+        /// <param name="TotalCost">The total cost of the invoice</param>
+        /// <returns></returns>
+        public string SelectInvoiceByDateAndCost(string InvoiceNum, string InvoiceDate, string TotalCost)
+        {
+            string sSQL = "SELECT * FROM Invoices WHERE InvoiceNum = " + InvoiceNum + " AND InvoiceDate = #" + InvoiceDate + "# AND TotalCost = " + TotalCost;
+
+            return sSQL;
+        }
+
+        /// <summary>
+        /// The SQL to select the invoices by total cost.
+        /// </summary>
+        /// <param name="TotalCost">The total cost of the invoice</param>
+        /// <returns></returns>
+        public string SelectInvoiceByCost(string TotalCost)
+        {
+            string sSQL = "SELECT * FROM Invoices WHERE TotalCost =" + TotalCost;
+
+            return sSQL;
+        }
+
+        /// <summary>
+        /// The SQL to select the invoice by cost and date.
+        /// </summary>
+        /// <param name="TotalCost">The total cost of the invoice</param>
+        /// <param name="InvoiceDate">The invoice date</param>
+        /// <returns></returns>
+        public string SelectInvoiceByCostAndDate(string TotalCost, string InvoiceDate)
+        {
+            string sSQL = "SELECT * FROM Invoices WHERE TotalCost =" + TotalCost + "AND InvoiceDate = " + InvoiceDate;
+
+            return sSQL;
+        }
+
+        /// <summary>
+        /// The SQL to select the invoices by the date.
+        /// </summary>
+        /// <param name="InvoiceDate">The invoice date</param>
+        /// <returns></returns>
+        public string SelectInvoiceByDate(string InvoiceDate)
+        {
+            string sSQL = "SELECT * FROM Invoices WHERE InvoiceDate = " + InvoiceDate;
+
+            return sSQL;
+
+        }
+
+
+
+
+
+
     }
 }
