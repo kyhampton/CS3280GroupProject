d2ed5f40be6661fb50021013166302a25460ea69
Hunter Mikkelsen <hunter.mikk@gmail.com>
2021-04-02 13:10:41 -0600

    Added / Updated comments in each SQL class. Created Regions to chunk out code.

--- .vs/GroupProject/GroupProject/GroupProject/Items/clsItemsSQL.cs
+++ .vs/GroupProject/GroupProject/GroupProject/Items/clsItemsSQL.cs
@@ -14,7 +14,7 @@ namespace GroupProject.Items
         /// <summary>
         /// Returns a SQL statement to selects all the availalbe items.
         /// </summary>
-        /// <returns></returns>
+        /// <returns>All data from the list of items</returns>
         public string SelectAllItems()
         {
             string sSQL = "SELECT ItemCode, ItemDesc, Cost FROM ItemDesc";
@@ -27,7 +27,7 @@ namespace GroupProject.Items
         /// </summary>
         /// <param name="InvoiceNum"></param>
         /// <param name="ItemCode"></param>
-        /// <returns></returns>
+        /// <returns>A specific item from the list of items</returns>
         public string SelectSpecificItem(string InvoiceNum, string ItemCode)
         {
             string sSQL = "SELECT distinct(" + InvoiceNum + ") FROM LineItems WHERE ItemCode = " + ItemCode;
@@ -41,7 +41,7 @@ namespace GroupProject.Items
         /// <param name="Description"></param>
         /// <param name="Cost"></param>
         /// <param name="ItemCode"></param>
-        /// <returns></returns>
+        /// <returns>An updated description / cost of an item</returns>
         public string UpdateItemDescription(string Description, string Cost, string ItemCode)
         {
             string sSQL = "UPDATE ItemDesc SET ItemDesc = '" + Description + "', Cost = " + Cost + " WHERE ItemCode = '" + ItemCode +"'";
@@ -55,7 +55,7 @@ namespace GroupProject.Items
         /// <param name="ItemCode"></param>
         /// <param name="ItemDesc"></param>
         /// <param name="Cost"></param>
-        /// <returns></returns>
+        /// <returns>A new item</returns>
         public string CreateItem(string ItemCode, string ItemDesc, string Cost)
         {
             string sSQL = "INSERT INTO ItemDesc (ItemCode, ItemDesc, Cost) VALUES ('" + ItemCode + "', '" + ItemDesc + "', " + Cost + ")";
